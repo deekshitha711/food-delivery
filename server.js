3504f5d80a8ae95e4af0611e8ea8636fda314f14
Deekshitha <deekshithaemandi2007@gmail.com>
2026-04-02 22:44:23 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(cors({ origin: "https://food-delivery-page1.netlify.app/", credentials: true }));
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
